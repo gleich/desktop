dbd3a353e7de58308ee4250a313f1603d7435ab4
Matthew Gleich <matthewgleich@gmail.com>
2020-07-26 22:10:16 -0400

    ♻️ Refactor to return errors
    Signed-off-by: Matthew Gleich <matthewgleich@gmail.com>

--- go.mod
+++ go.mod
@@ -1,9 +1,3 @@
 module github.com/Matt-Gleich/go_template
 
 go 1.14
-
-require (
-	github.com/Matt-Gleich/statuser/v2 v2.0.1
-	github.com/mattn/go-colorable v0.1.7 // indirect
-	golang.org/x/sys v0.0.0-20200724161237-0e2f3a69832c // indirect
-)
--- macos.go
+++ macos.go
@@ -1,25 +1,21 @@
 package runningapps
 
 import (
+	"errors"
 	"os/exec"
 	"runtime"
 	"strings"
-
-	"github.com/Matt-Gleich/statuser/v2"
 )
 
 // MacOSApplications ... Get a list of all running desktop application for a mac system
-func MacOSApplications() []string {
-	if runtime.GOOS != "darwin" {
-		statuser.ErrorMsg("MacOSApplications function only supported on a darwin based system", 1)
+func MacOSApplications() ([]string, error) {
+	if err := checkMacOS(); err != nil {
+		return []string{}, err
 	}
-
 	out, err := exec.Command("osascript", "-e", `tell application "System Events" to get name of (processes where background only is false)`).Output()
 	if err != nil {
 		err := exec.Command("osascript", "-e", `tell application "System Events" to activate`).Run()
-		if err != nil {
-			statuser.Error("Failed to get running list of applications", err, 1)
-		}
+		return []string{}, err
 	}
 
 	dirtyApplications := strings.Split(string(out), ",")
@@ -29,17 +25,22 @@ func MacOSApplications() []string {
 			cleanApplications = append(cleanApplications, strings.TrimSpace(app))
 		}
 	}
+	return cleanApplications, nil
+}
 
 // MacOSQuitApp ... Quit a desktop application for a mac system
 func MacOSQuitApp(name string) error {
-	checkMacOS()
+	if err := checkMacOS(); err != nil {
+		return err
+	}
 	cleanedName := strings.ReplaceAll(name, " ", "\\ ")
 	err := exec.Command("pkill", "-x", cleanedName).Run()
 	return err
 }
 
-func checkMacOS() {
+func checkMacOS() error {
 	if runtime.GOOS != "darwin" {
-		statuser.ErrorMsg("MacOSApplications function only supported on a darwin based system", 1)
+		return errors.New("Wrong OS, only darwin system supported")
 	}
+	return nil
 }
